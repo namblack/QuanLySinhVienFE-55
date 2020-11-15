cf8fe81537d761874dbbbdef1b2c58045ff1aefa
MANCHESTER 4EVER RED <ngthdan0811@gmail.com>
2020-11-15 10:17:48 +0700

    user b version 3 chinhsua lophoc.js

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,4 +1,8 @@
 var Lophoc = function () {
     this.maLop = '';
     this.tenLop = '';
+    this.danhSachSV = [];
+    this.hienThiThongTinLopHoc = function() {
+        console.log("hsakdjfhksadfh");
+    }
 }
